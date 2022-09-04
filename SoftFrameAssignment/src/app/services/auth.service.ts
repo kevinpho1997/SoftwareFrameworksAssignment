3e1b0427a8043c9f60306d2b55c4ef7865dfec47
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-05 01:24:42 +1000

    added auth service

--- SoftFrameAssignment/src/app/services/auth.service.ts
+++ SoftFrameAssignment/src/app/services/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // http login request
+  username: string;
+  password: string;
+  server: string = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.username = "";
+    this.password = "";
+  }
+
+  loginEvent(username: string, password: string) {
+    return this.http.post<User>(this.server + '/api/auth', {username: username, password: password});
+  }
 }
