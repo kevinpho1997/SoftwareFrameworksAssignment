5dbb4f199b73ce05a2580b1d215e7cf0c0be345c
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-06 00:47:32 +1000

    added register routing

--- SoftFrameAssignment/src/app/app-routing.module.ts
+++ SoftFrameAssignment/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { ChatComponent } from './chat/chat.component';
 import { GroupComponent } from './group/group.component';
 import { ChannelComponent } from './channel/channel.component';
 import { AuthGuard } from './guards/auth.guard';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent}, 
   {path: 'chat', component: ChatComponent, canActivate: [AuthGuard]}, 
   {path: 'group', component: GroupComponent, canActivate: [AuthGuard]}, 
   {path: 'channel', component: ChannelComponent, canActivate: [AuthGuard]}, 
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
