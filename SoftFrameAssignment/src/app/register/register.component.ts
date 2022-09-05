3dd726f2347851ac4ffc0ac85455b0a005c45da5
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-06 04:08:12 +1000

    added register form added register user service added register component code (temp)

--- SoftFrameAssignment/src/app/register/register.component.ts
+++ SoftFrameAssignment/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserAdminService } from '../services/user-admin.service';
 
 @Component({
   selector: 'app-register',
@@ -6,12 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  username = '';
+  email = '';
   id: number;
   birthdate: string;
   role: string;
   pword: string;
 
-  constructor() {
+  constructor(private router: Router, private uAdminServ: UserAdminService) {
     this.id = 0;
     this.birthdate = "2022-01-01";
     this.role = "default";
@@ -21,8 +25,15 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  registerClicked() {
-
+  registerClicked(event: any) {
+    event.preventDefault();
+    this.uAdminServ.registerUser(
+      this.username, this.email, this.id, this.birthdate, this.role)
+        .subscribe(
+          (data: any) => {
+            console.log(data);
+          }
+        )
   }
 
 }
