b0d1c220f3d5a6a828881312fbb756f26bf008fe
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-06 22:27:10 +1000

    added creation of a user - writing + reading to json files

--- SoftFrameAssignment/server/routes/registerUser.js
+++ SoftFrameAssignment/server/routes/registerUser.js
@@ -19,12 +19,39 @@ module.exports = function(app) {
         fs.readFile('./data/userInfo.json', 'utf8', function(err, data) {
             if (err) throw err;
             uArray= JSON.parse(data);
+            userIdLength = uArray.length + 1;
             console.log("uArray", uArray);
             console.log("userObj", userObj);
             console.log("userInfoObj", userInfoObj);
+            console.log("uarrayLength", userIdLength);
 
-            
-
+            let i = uArray.findIndex(user => user.username == userInfoObj.username);
+            if (i == -1) {
+                userInfoObj.id = userIdLength;
+                uArray.push(userInfoObj);
+                // writing to userInfo.json
+                let uArrayJson = JSON.stringify(uArray);
+                fs.writeFile('./data/userInfo.json', uArrayJson, 'utf8', function(err){
+                    if (err) throw err;
+                });
+                // reading users.json
+                fs.readFile('./data/users.json', 'utf8', function(err, data){
+                    if (err) throw err;
+                    let userLoginInfo = JSON.parse(data);
+                    userLoginInfo.push(userObj);
+                    let usersJson = JSON.stringify(userLoginInfo);
+                    // writing to users.json
+                    fs.writeFile('./data/users.json', usersJson, 'utf8', function(err){
+                        if (err) throw err;
+                    });
+                })
+                // for handling if the user exists or not
+                uArray["exists"] = false;
+            } else {
+                // res.send(uArray);
+                uArray["exists"] = true;
+            }
+            res.send(uArray);
         });
     });
 }
