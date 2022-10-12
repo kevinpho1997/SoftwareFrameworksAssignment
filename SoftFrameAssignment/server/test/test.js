ae15a50ce97529d7f68a2942f15b3ccf75f5f43d
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-10-12 20:51:51 +1000

    first test passes

--- SoftFrameAssignment/server/test/test.js
+++ SoftFrameAssignment/server/test/test.js
@@ -4,6 +4,7 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 // const { get} = require('http');
 chai.use(chaiHttp);
+let should = chai.should();
 
 describe('Server integration test', function() {
     // The function passed to before() is called before running the test cases.
@@ -15,4 +16,32 @@ describe('Server integration test', function() {
         console.log("after test");
         // run mongo here to delete things in database
     });
+    // test get all users
+    describe('/users', () => {
+        it('it should GET all users', (done) => {
+            chai.request(app).get('/users')
+                //.get('./routes/getProdList')
+                .end((err, res) => {
+                    // console.log("RESPONSE", res.body);
+                    // should.exist(res.body);
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+    // test register user
+    // describe('/user/create', () => {                                            
+    //     it('it should insert a doc (user)', (done) => {
+    //         chai.request(app)
+    //             .post('/user/create').type('form').send({ 'username': "newUsername", "userid": 30, 'email': "email@email.com", 'description': 'My description', "birthdate": "2021-01-01", "userage":100, "role":"super"})
+    //             .end((err, res) => {
+    //                 // test 1
+    //                 res.should.have.status(200);
+    //                 //  test 2
+    //                 res.body.should.have.property('valid').to.not.be.false;
+    //                 done();
+    //             });
+    //     });
+    // });
 });
