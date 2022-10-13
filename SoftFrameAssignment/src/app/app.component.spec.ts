9805831aee7edbcc4824544b3d5b50ecc7896179
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-10-13 23:21:44 +1000

    tried updating karma test file

--- SoftFrameAssignment/src/app/app.component.spec.ts
+++ SoftFrameAssignment/src/app/app.component.spec.ts
@@ -1,12 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { ChannelComponent } from './channel/channel.component';
+import { ChatComponent } from './chat/chat.component';
+import { GroupComponent } from './group/group.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 
 describe('AppComponent', () => {
+  const routes: Routes = [
+    {path: 'login', component: LoginComponent}, 
+    {path: 'chat', component: ChatComponent, canActivate: [AuthGuard]}, 
+    {path: 'group', component: GroupComponent, canActivate: [AuthGuard]}, 
+    {path: 'channel', component: ChannelComponent, canActivate: [AuthGuard]}, 
+    {path: 'register', component: RegisterComponent, canActivate: [AuthGuard]}
+  ];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        ChatComponent,
+        GroupComponent,
+        ChannelComponent,
+        RegisterComponent
       ],
+      imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ]
     }).compileComponents();
   });
 
