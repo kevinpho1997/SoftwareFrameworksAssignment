bdb6428d70f87408842d339caab77ad769bdf449
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-10-13 22:03:39 +1000

    tried adding localstorage command -didn't work added more tests

--- SoftFrameAssignment/cypress.config.ts
+++ SoftFrameAssignment/cypress.config.ts
@@ -4,7 +4,11 @@ export default defineConfig({
   
   e2e: {
     'baseUrl': 'http://localhost:4200',
-    supportFile: false
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      require("cypress-localstorage-commands/plugin")(on, config);
+      return config;
+    }
   },
   
   
--- SoftFrameAssignment/cypress/e2e/spec.cy.ts
+++ SoftFrameAssignment/cypress/e2e/spec.cy.ts
@@ -1,30 +1,44 @@
 describe('My First Test', () => {
+  it('Visits the login page', () => {
+    cy.visit('http://localhost:4200/login');
+    cy.get('h1').contains("Login into your account")
+  })
+})
+
+describe('My First Test', () => {
   function login() {
     cy.visit('http://localhost:4200/login');
     cy.get('h1').contains("Login into your account")
     cy.get('input[name=username]').type("kevin")
     cy.get('input[name=password]').type("123")
     cy.get('button[name=sign-in-button]').click()
+    // cy.saveLocalStorage();
   }
+  beforeEach(() => {
+    login()
+    // cy.setLocalStorage("username", "kevin");
+    // cy.restoreLocalStorage();
+  });
+
   it('Visits the login page', () => {
     cy.visit('http://localhost:4200/login');
     cy.get('h1').contains("Login into your account")
   })
   it('logout', () => {
-    login();
     cy.get("a[name=logout-button]").click()
     cy.get('h1').contains("Login into your account")
   })
   it('Visit Chat page', () => {
-    login();
     cy.get('h1').contains('Users')
   })
   it('should contain a table of users', () => {
-    login();
     cy.get('table').contains('super')
     cy.get('table').contains('kevin')
     cy.get('table').contains('yeji')
     cy.get('table').contains('dahyun')
   })
-  
+  it('should visit register page', () => {
+    cy.visit('http://localhost:4200/register');
+    // cy.get('h1').contains("Register a User")
+  })
 })
