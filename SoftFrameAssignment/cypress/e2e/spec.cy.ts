723c94b49ff06d2d80400634587e7ae812d50e1b
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-10-13 20:45:11 +1000

    first 2 cypress tests pass

--- SoftFrameAssignment/cypress/e2e/spec.cy.ts
+++ SoftFrameAssignment/cypress/e2e/spec.cy.ts
@@ -1,6 +1,19 @@
 describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
+  it('Visits the login page', () => {
+    cy.visit('http://localhost:4200/login');
+    cy.get('h1').contains("Login into your account")
+    cy.get('input[name=username]').type("kevin")
+    cy.get('input[name=password]').type("123")
+    cy.get('button[name=sign-in-button]').click()
+    // cy.get('form')
+    // cy.get('form').contains('loginClicked($event)').click()
+  })
+  it('Visit Chat page', () => {
+    cy.visit('http://localhost:4200/login');
+    cy.get('h1').contains("Login into your account")
+    cy.get('input[name=username]').type("kevin")
+    cy.get('input[name=password]').type("123")
+    cy.get('button[name=sign-in-button]').click()
+    cy.get('h1').contains('Users')
   })
 })
