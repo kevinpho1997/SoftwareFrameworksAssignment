c45dc9f420993f5afb6c5d1e47911ee8e1066350
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-10-13 20:48:51 +1000

    made login into a function

--- SoftFrameAssignment/cypress/e2e/spec.cy.ts
+++ SoftFrameAssignment/cypress/e2e/spec.cy.ts
@@ -1,19 +1,26 @@
 describe('My First Test', () => {
-  it('Visits the login page', () => {
+  function login() {
     cy.visit('http://localhost:4200/login');
     cy.get('h1').contains("Login into your account")
     cy.get('input[name=username]').type("kevin")
     cy.get('input[name=password]').type("123")
     cy.get('button[name=sign-in-button]').click()
+  }
+  it('Visits the login page', () => {
+    login();
     // cy.get('form')
     // cy.get('form').contains('loginClicked($event)').click()
   })
   it('Visit Chat page', () => {
-    cy.visit('http://localhost:4200/login');
-    cy.get('h1').contains("Login into your account")
-    cy.get('input[name=username]').type("kevin")
-    cy.get('input[name=password]').type("123")
-    cy.get('button[name=sign-in-button]').click()
+    login();
     cy.get('h1').contains('Users')
   })
+  // it('should contain a table of users', () => {
+  //   cy.visit('http://localhost:4200/login');
+  //   cy.get('h1').contains("Login into your account")
+  //   cy.get('input[name=username]').type("kevin")
+  //   cy.get('input[name=password]').type("123")
+  //   cy.get('button[name=sign-in-button]').click()
+
+  // })
 })
