790ad4afa4a7e57329fc34b2b2d36930b83722aa
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-10-13 21:02:02 +1000

    added logout and getuser test

--- SoftFrameAssignment/cypress/e2e/spec.cy.ts
+++ SoftFrameAssignment/cypress/e2e/spec.cy.ts
@@ -7,20 +7,24 @@ describe('My First Test', () => {
     cy.get('button[name=sign-in-button]').click()
   }
   it('Visits the login page', () => {
+    cy.visit('http://localhost:4200/login');
+    cy.get('h1').contains("Login into your account")
+  })
+  it('logout', () => {
     login();
-    // cy.get('form')
-    // cy.get('form').contains('loginClicked($event)').click()
+    cy.get("a[name=logout-button]").click()
+    cy.get('h1').contains("Login into your account")
   })
   it('Visit Chat page', () => {
     login();
     cy.get('h1').contains('Users')
   })
-  // it('should contain a table of users', () => {
-  //   cy.visit('http://localhost:4200/login');
-  //   cy.get('h1').contains("Login into your account")
-  //   cy.get('input[name=username]').type("kevin")
-  //   cy.get('input[name=password]').type("123")
-  //   cy.get('button[name=sign-in-button]').click()
-
-  // })
+  it('should contain a table of users', () => {
+    login();
+    cy.get('table').contains('super')
+    cy.get('table').contains('kevin')
+    cy.get('table').contains('yeji')
+    cy.get('table').contains('dahyun')
+  })
+  
 })
